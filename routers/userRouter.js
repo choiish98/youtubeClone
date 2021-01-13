4fec7c1e85b325ddb2918e735b7e486754f40f56
choiish98 <56579239+choiish98@users.noreply.github.com>
2021-01-14 06:57:10 +0900

    #2.10 plus
    doing same thing in userRouter.js and videoRouter.js

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -2,4 +2,9 @@ import express from "express";
 
 const userRouter = express.Router();
 
+userRouter.get(routes.users, (req, res) => res.send('USERS'));
+userRouter.get(routes.userDetail, (req, res) => res.send('USER_DETAIL'));
+userRouter.get(routes.editProfile, (req, res) => res.send('EDIT_PROFILE'));
+userRouter.get(routes.changePassword, (req, res) => res.send('CHANGE_PASSWORD'));
+
 export default userRouter;
--- routers/videoRouter.js
+++ routers/videoRouter.js
@@ -2,4 +2,10 @@ import express from "express";
 
 const videoRouter = express.Router();
 
+videoRouter.get(routes.videos, (req, res) => res.send('VIDEOS'));
+videoRouter.get(routes.upload, (req, res) => res.send('UPLOAD'));
+videoRouter.get(routes.videoDetail, (req, res) => res.send('VIDEOS_DETAIL'));
+videoRouter.get(routes.editVideo, (req, res) => res.send('EDIT_VIDEO'));
+videoRouter.get(routes.deleteVideo, (req, res) => res.send('DELETE_VIDEO'));
+
 export default videoRouter;
