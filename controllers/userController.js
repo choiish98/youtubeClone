b2869387f0fbb7e7ceeecc2e6cb924fa80716c75
choiish98 <56579239+choiish98@users.noreply.github.com>
2021-01-14 07:19:05 +0900

    #2.11

---
+++ controllers/userController.js
@@ -0,0 +1,8 @@
+export const join = (req, res) => res.send("Join");
+export const login = (req, res) => res.send("Login");
+export const logout = (req, res) => res.send("Logout");
+
+export const users = (req, res) => res.send('USERS');
+export const userDetail = (req, res) => res.send('USER_DETAIL');
+export const editProfile = (req, res) => res.send('EDIT_PROFILE');
+export const changePassword = (req, res) => res.send('CHANGE_PASSWORD');
